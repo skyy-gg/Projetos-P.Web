92f7144bc7f542b19ad5b253bd467bb9797594ae
Skyy <117464525+SkyyPG@users.noreply.github.com>
2026-08-13 09:59:29 -0300

    Alteração e instalação do Express - Dia 13/08

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = require('./src');
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("API funcionando");
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Servidor iniciado na porta ${PORT}`);
+});
